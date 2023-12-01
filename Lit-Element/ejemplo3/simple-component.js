bc220696c6e042a07a459e3b3bbeba357a7583cf
Guillermo Cuenca <guillermo.cuenca.macedo@gmail.com>
2023-11-30 20:39:14 -0600

    Create simple-component.js

---
+++ Lit-Element/ejemplo3/simple-component.js
@@ -0,0 +1,52 @@
+import { LitElement,css, html } from 'lit';
+export class SimpleComponent extends LitElement {
+  static styles = css`
+    h1,p, button {
+      text-align: center;
+    }
+  `;
+  static get properties() {
+    return {
+      nombre: {type: String},
+      email: {type: String},
+      verMas: {type: Boolean},
+    }
+  }
+  constructor() {
+    super();
+    this.verMas = false;
+  }
+
+  toggle(e){
+    e.preventDefault();
+    this.verMas = !this.verMas;
+  }
+
+  render() {
+    return html`
+      <style>
+        div {
+          border: 1px solid #ddd;
+          padding: 10px;
+          border-radius: 10px;
+          display: inline-block;
+        }
+
+        h1 {
+          font-size: 1.2rem;
+          font-weight: normal;
+        }
+      </style>
+      <div>
+        <h1>${this.nombre}</h1>
+        <p><a href="#" @click="${this.toggle}">Ver más información</a></p>
+        ${!this.verMas ?
+          html`Email de ${this.nombre}: ${this.email}`:
+          ''      
+        }
+      </div>
+    `;
+  }
+
+}
+customElements.define('simple-component', SimpleComponent);
