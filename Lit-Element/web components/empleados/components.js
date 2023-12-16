6dcfefaded54583fb70bff3eb046b86cfee550b4
Guillermo Cuenca <guillermo.cuenca.macedo@gmail.com>
2023-12-15 20:13:29 -0600

    Create components.js

---
+++ Lit-Element/web components/empleados/components.js
@@ -0,0 +1,11 @@
+import {Employees} from './employees-component.js'
+import {EmployeeTable} from './employee-table.js'
+import {ClearSearchButton} from './clear-search-button.js'
+import {SearchButton} from './search-button.js'
+import {SearchInput} from './search-input.js'
+
+customElements.define('empleados-component', Employees);
+customElements.define('employee-table', EmployeeTable);
+customElements.define('clear-search-button', ClearSearchButton);
+customElements.define('search-button', SearchButton);
+customElements.define('search-input', SearchInput);
